50008469dd31b15b061e056f8b9f63bb5180b2c0
nanxung <418732021@qq.com>
2018-06-18 17:22:09 +0800

    fix

--- src/main/java/com/springmvc/controller/main.java
+++ src/main/java/com/springmvc/controller/main.java
@@ -1,36 +1,30 @@
 package com.springmvc.controller;
 
-import com.springmvc.pojo.Admin;
-import com.springmvc.service.AdminService;
+import com.springmvc.pojo.User;
+import com.springmvc.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 import javax.annotation.Resource;
 
 @Controller
 public class main {
-
     @Resource
-    AdminService adminService;
-    @RequestMapping("/")
+    private UserService userService;
+    @RequestMapping(value = "/user",method = RequestMethod.POST)
     @ResponseBody
-    public String index(){
-        return "hello world";
+    public int insertUser(User user){
+        return userService.insertUser(user);
+    }
+    @RequestMapping(value = "/user/{id}",method = RequestMethod.GET)
+    @ResponseBody
+    public User getUser(@PathVariable Integer id){
+        return userService.getUser(id);
     }
 
-    @RequestMapping(value = "/admin",method = RequestMethod.POST)
-    @ResponseBody
-    public int insertUser(Admin admin){
-       return adminService.insertAdmin(admin);
-
-    }
-    @RequestMapping(value = "/admin/{id}",method = RequestMethod.GET)
-    @ResponseBody
-    public Admin getUser(@PathVariable  Integer id){
-        return adminService.getAdmin(id);
-
-    }
 }
